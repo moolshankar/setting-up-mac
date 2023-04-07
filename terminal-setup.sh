9591d492a7e7dd62c77714f87149393ab42905f8
Moolshankar Tyagi <moolshankartyagi@gmail.com>
2023-04-07 00:59:47 -0700

    [Initial] : Created file till ohMyZsh install

---
+++ terminal-setup.sh
@@ -0,0 +1,22 @@
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install iTerm2
+brew install --cask iterm2
+
+# Close default terminal and open iTerm
+
+# Install git if not installed
+brew install git
+git --version
+
+# Install ohMyZsh
+
+# Using curl
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Or using wget
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# Install ohMyZsh powerlevel10k theme from https://github.com/romkatv/powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
