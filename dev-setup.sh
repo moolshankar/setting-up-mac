cbf6b839919537d1e5790e34dd18f95c723d03f3
Moolshankar Tyagi <moolshankartyagi@gmail.com>
2023-04-07 05:17:25 -0700

    Create dev-setup.sh

---
+++ dev-setup.sh
@@ -0,0 +1,19 @@
+# Install Python3
+brew install python3
+
+# Install Nodejs
+brew install node
+
+# Install Golang
+brew install go
+
+# Install Docker
+brew install --cask docker
+open /Applications/Docker.app
+
+# Install Minikube
+brew install minikube
+
+# Install VSCode
+# Direct download from https://code.visualstudio.com/docs/setup/mac
+
